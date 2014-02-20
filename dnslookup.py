b43918ed932fc1885bc47e92eaa457e7f2125493
Luis Uranga <lm.uranga@gmail.com>
2014-02-20 05:12:15 -0200

    Update dnslookup.py

--- dnslookup.py
+++ dnslookup.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+'''
+    python dnslookup.py www.lanacion.com.ar www.telpin.com.ar
+    www.telpin.com.ar : 201.219.64.50
+    www.lanacion.com.ar : 23.21.247.181
+'''
 
 from dnslookup import lookup
 import sys
 
 domain = sys.argv[1:]
 
+
 for x in domain:
     dns = lookup(x)
     print "{0} : {1}".format(x, dns)
-    
-
-
-# eg.
-# python dnslookup.py www.somehost1.com www.somehost2.com www.somehostn.com

f1ce3846f6decdf36ec6cde526908ccd7984ed61
Luis Uranga <lm.uranga@gmail.com>
2014-02-20 05:11:07 -0200

    Update dnslookup.py

--- dnslookup.py
+++ dnslookup.py
@@ -5,5 +5,12 @@ from dnslookup import lookup
 import sys
 
 domain = sys.argv[1:]
-dns = lookup(domain[0])
-print "{0} : {1}".format(domain, dns)
+
+for x in domain:
+    dns = lookup(x)
+    print "{0} : {1}".format(x, dns)
+    
+
+
+# eg.
+# python dnslookup.py www.somehost1.com www.somehost2.com www.somehostn.com
